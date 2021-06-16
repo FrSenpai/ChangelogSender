9262a0b774e25f0d335b030fea95845108a9a9ce
Alex <collo@DESKTOP-0MTRO7E>
2021-06-16 12:32:52 +0200

    starting implementation of authentification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './components/layouts/top-bar/top-bar.component'
-// Firebase App (the core Firebase SDK) is always required and
-// must be listed before other Firebase SDKs
-import firebase from "firebase/app";
+import { AuthentificationService } from './services/authentification.service'
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
-// Add the Firebase services that you want to use
-import "firebase/auth";
-import "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyA0aT0mJR5ZscjKAqah6sEEn1NmKF7lYT8",
   authDomain: "changelogsender.firebaseapp.com",
@@ -19,7 +18,7 @@ const firebaseConfig = {
   messagingSenderId: "320974003738",
   appId: "1:320974003738:web:f59d87502dcb84343cfba3"
 };
-firebase.initializeApp(firebaseConfig)
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -28,9 +27,12 @@ firebase.initializeApp(firebaseConfig)
   imports: [
     BrowserModule,
     AppRoutingModule,
-
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage
   ],
-  providers: [],
+  providers: [AuthentificationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/authentification.service.ts
+++ src/app/services/authentification.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { BehaviorSubject } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthentificationService {
+  user$: BehaviorSubject<any> = new BehaviorSubject(null)
+  constructor(private fireAuth: AngularFireAuth) { 
+    this.fireAuth.authState.subscribe((res) => {
+      console.log(res)
+      this.user$.next(res)
+      console.log(this.isConnected())
+    })
+  }
 
-  constructor() { }
+  public isConnected():boolean {
+    return this.user$.value !== null
+  }
 }
